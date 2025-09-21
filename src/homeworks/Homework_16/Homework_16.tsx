42412f9e76499bcf2a451c994f4a120aefdfef96
Eugen3113 <eg31it@gmail.com>
2025-09-21 21:32:34 +0200

    add feedbackSlice

--- src/homeworks/Homework_16/Homework_16.tsx
+++ src/homeworks/Homework_16/Homework_16.tsx
@@ -1,16 +1,20 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { incrementLike, incrementDislike, resetFeedback } from 'store/redux/feedback/feedbackSlice';
-import { RootState } from 'store/store';
-import Feedback from 'components/Feedback/Feedback';
-import { PageWrapper } from './styles';
+import { useDispatch, useSelector } from "react-redux"
+import {
+  incrementLike,
+  incrementDislike,
+  resetFeedback,
+} from "store/redux/feedback/feedbackSlice"
+import { RootState } from "store/store"
+import Feedback from "components/Feedback/Feedback"
+import { PageWrapper } from "./styles"
 
 function Homework_16() {
-  const dispatch = useDispatch();
-  const { like, dislike } = useSelector((state: RootState) => state.FEEDBACK);
+  const dispatch = useDispatch()
+  const { like, dislike } = useSelector((state: RootState) => state.FEEDBACK)
 
-  const onLike = () => dispatch(incrementLike());
-  const onDislike = () => dispatch(incrementDislike());
-  const resetResults = () => dispatch(resetFeedback());
+  const onLike = () => dispatch(incrementLike())
+  const onDislike = () => dispatch(incrementDislike())
+  const resetResults = () => dispatch(resetFeedback())
 
   return (
     <PageWrapper>
@@ -22,8 +26,7 @@ function Homework_16() {
         resetResults={resetResults}
       />
     </PageWrapper>
-  );
+  )
 }
 
-export default Homework_16;
-
+export default Homework_16
--- src/store/redux/feedback/feedbackSlice.ts
+++ src/store/redux/feedback/feedbackSlice.ts
@@ -1,32 +1,32 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { FeedbackSliceState } from "./types"
+import { createAppSlice } from "store/createAppSlice"
 
 interface FeedbackState {
-  like: number;
-  dislike: number;
+  like: number
+  dislike: number
 }
 
 const initialState: FeedbackState = {
   like: 0,
   dislike: 0,
-};
+}
 
-export const feedbackSlice = createSlice({
-  name: 'FEEDBACK',
+export const feedbackSlice = createAppSlice({
+  name: "FEEDBACK",
   initialState,
-  reducers: {
-    incrementLike(state) {
-      state.like += 1;
-    },
-    incrementDislike(state) {
-      state.dislike += 1;
-    },
-    resetFeedback(state) {
-      state.like = 0;
-      state.dislike = 0;
-    },
-  },
-});
+  reducers: create => ({
+    incrementLike: create.reducer((state: FeedbackState) => {
+      state.like += 1
+    }),
+    incrementDislike: create.reducer((state: FeedbackState) => {
+      state.dislike += 1
+    }),
+    resetFeedback: create.reducer((state: FeedbackState) => {
+      state.like = 0
+      state.dislike = 0
+    }),
+  }),
+})
 
-export const { incrementLike, incrementDislike, resetFeedback } = feedbackSlice.actions;
-export default feedbackSlice.reducer;
+export const { incrementLike, incrementDislike, resetFeedback } =
+  feedbackSlice.actions
+export default feedbackSlice.reducer
