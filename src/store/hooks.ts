cb63af0fc0acc459f6df99a0cc2a6f7c79ca5023
Eugen3113 <eg31it@gmail.com>
2025-09-22 19:19:10 +0200

    add types.ts

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -4,9 +4,16 @@
 // We disable the ESLint rule here because this is the designated place
 // for importing and re-exporting the typed versions of hooks.
 /* eslint-disable no-restricted-imports */
+// Этот файл служит центральным узлом для реэкспорта предварительно типизированных хуков Redux.
+// Этот импорт ограничен в других местах, чтобы обеспечить единообразное
+// использование типизированных хуков во всем приложении.
+// Мы отключаем здесь правило ESLint, поскольку это назначенное место
+// для импорта и реэкспорта типизированных версий хуков.
+/* eslint-disable no-restricted-imports */
 import { useDispatch, useSelector } from "react-redux"
 import type { AppDispatch, RootState } from "./store"
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
+// Используйте во всем приложении вместо обычного `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
 export const useAppSelector = useSelector.withTypes<RootState>()
--- src/store/redux/feedback/types.ts
+++ src/store/redux/feedback/types.ts
@@ -1,3 +1,4 @@
-export interface FeedbackSliceState {
-  count: number
+export interface FeedbackState {
+  like: number
+  dislike: number
 }
